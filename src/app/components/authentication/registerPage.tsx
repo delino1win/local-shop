c0db52d663a4719146677422d0abdbc5bb40c723
delino1win <delino1win@gmail.com>
2024-04-05 03:30:12 +0700

    register page fetch url fix

--- src/app/components/authentication/registerPage.tsx
+++ src/app/components/authentication/registerPage.tsx
@@ -32,7 +32,7 @@ const RegisterPage = () => {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/register", {
+      const res = await fetch("/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
