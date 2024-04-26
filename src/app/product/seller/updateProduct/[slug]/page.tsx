643d53fd24deb5fc2ed28cd91f03ff6e74f7dcae
delino1win <delino1win@gmail.com>
2024-04-26 21:18:44 +0700

    update form revamp api fetching to server way

--- src/app/product/seller/updateProduct/[slug]/page.tsx
+++ src/app/product/seller/updateProduct/[slug]/page.tsx
@@ -1,15 +1,31 @@
 import UpdateProductForm from "@/app/components/product/updateProduct";
-import getProductDetail from "@/utils/getProductDetail";
+import connectMongoDataBase from "@/libs/mongodb";
+import Product from "@/models/product";
+import { error } from "console";
 
 interface Params {slug: string}
+
+const getProductDetail = async (slug: string) => {
+  await connectMongoDataBase()
+  try {
+      const productDetail = await Product.findOne({_id: slug}).lean()
+
+      if(!productDetail) return 
+
+      return productDetail
+  } catch (error) {
+      console.log(error)
+  }
+}
 
 const UpdateProduct = async ({params} : {params: Params}) => {
   const { slug } = params;
   const productDetail = await getProductDetail(slug);
 
+  if(!productDetail) return ""
+
   // console.log("slug", slug)
   // console.log("Product Detail", productDetail)
-  
 
   return (
     <>
